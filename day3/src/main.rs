0f9f9b73ffaf8d3b644044a2a9b79192d8360ac1
Graham Lowe <glowe@users.noreply.github.com>
2022-12-03 15:17:36 -0500

    Add day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,50 +1,47 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io;
 
-fn priority(ch: &char) -> Option<u32> {
-    if *ch >= 'A' && *ch <= 'Z' {
-        return Some(*ch as u32 - 38);
+fn priority(item: &char) -> Option<u32> {
+    if *item >= 'A' && *item <= 'Z' {
+        return Some(*item as u32 - 38);
     }
-    if *ch >= 'a' && *ch <= 'z' {
-        return Some(*ch as u32 - 96);
+    if *item >= 'a' && *item <= 'z' {
+        return Some(*item as u32 - 96);
     }
     None
 }
 
+const GROUP_SIZE: u8 = 3;
+
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let mut priorities = 0;
-    loop {
+    'out: loop {
         let mut buf = String::new();
-        let n = stdin.read_line(&mut buf)?;
-        if n == 0 {
-            break;
-        }
-        let mut set = HashSet::new();
-        let buf = buf.trim_end();
-        let mid = buf.len() / 2;
-        buf.chars().take(mid).for_each(|c| {
-            set.insert(c);
-        });
-        buf.chars().skip(mid).for_each(|c| {
-            if set.contains(&c) {
-                let p = priority(&c).unwrap();
-                priorities += p;
-                // Prevent double counting by removing char
-                set.remove(&c);
+
+        for _ in 0..GROUP_SIZE {
+            let n = stdin.read_line(&mut buf)?;
+            if n == 0 {
+                break 'out;
             }
-        });
+        }
+        let mut frequency = HashMap::new();
+
+        for rucksack in buf.split_whitespace() {
+            let mut set = HashSet::new();
+            for item in rucksack.chars() {
+                set.insert(item);
+            }
+            for item in set.into_iter() {
+                let freq = frequency.entry(item).or_insert(0);
+                *freq += 1;
+                if *freq == GROUP_SIZE {
+                    priorities += priority(&item).unwrap();
+                }
+            }
+        }
     }
     println!("{}", priorities);
     Ok(())
 }
-
-// adding priority 16 for p
-// adding priority 38 for L
-// adding priority 38 for L
-// adding priority 42 for P
-// adding priority 22 for v
-// adding priority 22 for v
-// adding priority 20 for t
-// adding priority 20 for t
-// adding priority 19 for s
